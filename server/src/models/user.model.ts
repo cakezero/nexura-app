75e08f9d2cf0e58c3313423846c49ede5305a988
cakezero <sareahj.com@gmail.com>
2025-12-18 23:48:15 +0100

    used address instead of username

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -3,6 +3,11 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema({
 		username: {
 			type: String,
+			// required: true,
+			// unique: true
+		},
+		address: {
+			type: String,
 			required: true,
 			unique: true
 		},
