5e150c037deab3ddf020f029a6c6828b601f42a2
cakezero <sareahj.com@gmail.com>
2025-12-28 11:33:08 +0100

    created discord bot listener to listen for first messages and save them

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { port } from "@/utils/env.utils";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import helmet from "helmet";
+import { port, BOT_TOKEN } from "@/utils/env.utils";
 import DB from "@/config/db";
 import logger from "@/config/logger";
 import appRoutes from "@/routes";
-import helmet from "helmet";
+import { firstMessage } from "@/models/msg.model";
 
 const server = express();
 
@@ -15,7 +17,40 @@ server.use(express.urlencoded({ extended: true }));
 
 server.use("/api", appRoutes);
 
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	]
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+	logger.info(`Logged in as ${readyClient.user?.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+	console.log({ content: message.content, author: message.author.tag });
+	return;
+	if (message.author.bot) return;
+	if (!message.guild) return;
+
+	const user_id = message.author.id;
+	const guild_id = message.guild.id;
+	
+	const alreadySentMessage = await firstMessage.findOne({ user_id });
+
+	if (!alreadySentMessage) {
+		await firstMessage.create({
+			user_id,
+			guild_id
+		});
+	}
+});
+
 server.listen(port, async () => {
 	await DB();
+	await client.login(BOT_TOKEN);
 	logger.info(`Server is running on port ${port}`);
 });
