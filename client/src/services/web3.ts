9de4d8b346f73099367f5fd9c91cf81cc20dca9f
Bright Senpai <sareahj.com@gmail.com>
2026-02-28 11:15:31 +0000

    adjustments

--- client/src/services/web3.ts
+++ client/src/services/web3.ts
@@ -4,17 +4,17 @@ import { getWalletClient, getPublicClient } from "../lib/viem";
 
 // --- Deposit / Support or Oppose function ---
 export const buyShares = async (amountTrust: string, termId: Address, curveId = 1n) => {
-  const walletClient = getWalletClient();
+  const walletClient = await getWalletClient();
   const publicClient = getPublicClient();
 
   const { transactionHash } = await deposit(
-    { walletClient, publicClient, address: walletClient.account?.address }, // address?
+    { walletClient, publicClient }, // address?
     [
-      walletClient.account?.address, // receiver
+      walletClient?.account?.address, // receiver
       termId,                       // termId (atom or triple ID)
       curveId,                // curveId (use 1 for default curve - exponential, use 2 for linear)
       parseEther(amountTrust),               // assets (amount to deposit)
-    ]
+   ]
   )
 
   return transactionHash;
@@ -22,21 +22,16 @@ export const buyShares = async (amountTrust: string, termId: Address, curveId = 
 
 // --- Sell / Redeem ---
 export const sellShares = async (sharesAmount: string, termId: Address, curveId = 1n) => {
-  const walletClient = getWalletClient();
+  const walletClient = await getWalletClient();
   const publicClient = getPublicClient();
-
-  if (!walletClient || !walletClient?.account || !publicClient) {
-    throw new Error("wallet not installed or connected");
-  }
 
   const { transactionHash } = await redeem(
     { walletClient, publicClient, address: walletClient.account?.address },
     [
-      walletClient.account?.address, // receiver
+      walletClient?.account?.address, // receiver
       termId,                       // termId
       curveId,                      // curveId (use 1 for default curve)
       parseEther(sharesAmount),                // shares amount
-      0n,                          // minAssets (minimum assets to receive)
     ]
   );
 
