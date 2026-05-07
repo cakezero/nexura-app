d6c61ef6409e5cc8ebdb4f7ea95215432f9166a1
orion1125 <oyelolaayobami12345@gmail.com>
2026-05-07 19:07:43 +0000

    Make nameOfProject optional in save quest validation

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -95,7 +95,7 @@ export const validateCampaignData = (reqData: any) => {
 	const campaignSchema = z.object({
 		title: z.string().trim(),
 		description: z.string().trim(),
-		nameOfProject: z.string().trim(),
+		nameOfProject: z.string().trim().optional(),
 		starts_at: z.string().trim(),
 		ends_at: z.string().trim(),
 		reward: z.object({
