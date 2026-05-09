588a187905bbae1a4534bfd5ccdc269a5b173814
orion1125 <oyelolaayobami12345@gmail.com>
2026-05-09 22:40:00 +0000

    Remove temporary fix scripts

--- client/check.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let c = fs.readFileSync('src/pages/studio/user/UserSignup.tsx', 'utf8');
-console.log('handleSubmit at:', c.indexOf('handleSubmit'));
-console.log('handleSignUp at:', c.indexOf('handleSignUp'));
-console.log('handleSendOtp at:', c.indexOf('handleSendOtp'));
-console.log('OTP Step at:', c.indexOf('OTP Step'));
-console.log('OtpVerification at:', c.indexOf('OtpVerification'));
